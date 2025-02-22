cada0f85997d13da259bc0ad23259348225385de
octoper <vaggelisyfantis@gmail.com>
2025-02-22 03:35:43 +0200

    wip

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import Unfonts from 'unplugin-fonts/vite'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    Unfonts({
+      // Fontsource API
+      fontsource: {
+        /**
+         * Fonts families lists
+         */
+        families: [
+          'Inter Variable', // Require the `@fontsource-variable/inter` package to be installed.
+        ],
+      },
+    })
+  ],
 })
